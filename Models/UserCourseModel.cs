716eaad4a4b936011c134864974d990a3d4931ae
Oluwatobi <tellajames63@gmail.com>
2023-04-26 02:44:14 +0100

    Final

--- Models/UserCourseModel.cs
+++ Models/UserCourseModel.cs
@@ -1,10 +1,8 @@
 public class UserCourseModel
 {
-    public string Name { get; set; }
+   public string Name {get; set;}
 
-    
-    public string Email { get; set; }
+   public string Email {get; set;}
 
-    public List<string>Courses { get; set; }
-    public string ReferenceNumber { get; internal set; }
+   public List<string> Courses {get; set;}
 }
